61911b99722c7bae9dce502fb95d6c66eb1c32bc
Lauder01 <raulitoizg@gmail.com>
2025-07-22 13:20:16 +0200

    #5
    Añadir clase `Floor` con propiedades inicializadas
    
    Se ha creado una nueva clase `Floor` en `Floor.cs`, que incluye propiedades para `Id`, `FloorNumber`, `apartments` y `HasLift`. La propiedad `Id` se inicializa con un nuevo GUID, `FloorNumber` se establece en -666, `apartments` se inicializa como una lista vacía de `Apartment`, y `HasLift` se establece en `false`. También se incluye un constructor vacío.

---
+++ FQPClassLibraryProject/Floor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FQPClassLibraryProject
+{
+    public class Floor
+    {
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+        public int FloorNumber { get; set; } = -666;
+        public List<Apartment> apartments { get; set; } = new List<Apartment>();
+        public bool HasLift { get; set; } = false;
+        
+        public Floor() { }
+    }
+}
+
