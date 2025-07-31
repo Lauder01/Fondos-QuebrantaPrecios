052fd6d882d0d7cfa99f8eb77b983293489c2eb9
Lauder01 <raulitoizg@gmail.com>
2025-07-31 12:14:13 +0200

    Añadir proyecto RepositoryLibrary y interfaz IRepository
    Se ha añadido un nuevo proyecto "RepositoryLibraryProject" a la solución `FQP.sln`, incluyendo su configuración de compilación para Debug y Release.
    
    Se ha creado el archivo de proyecto `RepositoryLibraryProject.csproj` con el SDK de .NET y el marco de destino en .NET 8.0, además de una carpeta "DataAccessObjects".
    
    También se ha implementado la interfaz `IRepository<T>` en `IRepository.cs`, que proporciona métodos genéricos para operaciones CRUD sobre entidades de tipo `T`.

---
+++ RepositoryLibraryProject/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RepositoryLibraryProject.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(Guid id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+    }
+}
