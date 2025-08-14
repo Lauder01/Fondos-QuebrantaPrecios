f1c2234e860df07d23488e625330b623a23eea0f
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-14 12:41:26 +0200

    Creación landing page

--- FrontendApp/src/app/app.routes.ts
+++ FrontendApp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+
+export const routes: Routes = [
+	{ path: '', component: LandingComponent },
+];
+import { Routes } from '@angular/router';
