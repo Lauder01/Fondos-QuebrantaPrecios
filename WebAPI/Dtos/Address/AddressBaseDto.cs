82e72996b2db340d335ff0c90544d0460dd9aaf8
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-20 12:21:07 +0200

    Añadir validaciones a AddressBaseDto
    Se han agregado atributos de validación `[Range(0, 36)]` a las propiedades `BuildingId`, `ApartmentId` y `IsApartment` en la clase `AddressBaseDto`. Esto asegura que los valores de estas propiedades estén dentro del rango permitido, mejorando así la validación de los datos de entrada.

--- WebAPI/Dtos/Address/AddressBaseDto.cs
+++ WebAPI/Dtos/Address/AddressBaseDto.cs
@@ -6,8 +6,11 @@ namespace WebAPI.Dtos.Address
     public class AddressBaseDto : IValidatableObject
     {
         [Required]
+        [Range(0, 36)]
         public string BuildingId { get; set; } = string.Empty;
+        [Range(0, 36)]
         public string? ApartmentId { get; set; }
+        [Range(0, 36)]
         public bool? IsApartment { get; set; }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
