f52cc2c059f87ba9cf1966bdd254a98af95c9f29
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-21 10:43:22 +0200

    Elimina CreateRequestDto y mejora validaciones
    Se eliminó la clase `CreateRequestDto`. Se añadieron atributos de validación `[Required]` y `[Range]` en `RequestBaseDto` para las propiedades `BuildingId`, `StatusId`, `Price` y `MaintenancePrice`. Además, se incorporó una nueva propiedad `Id` en `RequestGetterDto` y `RequestUpdaterDto` con las mismas validaciones, mejorando así la validación de los datos de entrada en las solicitudes.

--- WebAPI/Dtos/Request/RequestGetterDto.cs
+++ WebAPI/Dtos/Request/RequestGetterDto.cs
@@ -2,6 +2,8 @@ namespace WebAPI.Dtos.Request
 {
     public class RequestGetterDto : RequestBaseDto
     {
+        [Required]
+        [Range(0, 36)]
         public string Id { get; set; } = string.Empty;
     }
 }
--- WebAPI/Dtos/Request/RequestUpdaterDto.cs
+++ WebAPI/Dtos/Request/RequestUpdaterDto.cs
@@ -5,6 +5,7 @@ namespace WebAPI.Dtos.Request
     public class RequestUpdaterDto : RequestBaseDto
     {
         [Required]
+        [Range(0,36)]
         public string Id { get; set; } = string.Empty;
     }
 }
