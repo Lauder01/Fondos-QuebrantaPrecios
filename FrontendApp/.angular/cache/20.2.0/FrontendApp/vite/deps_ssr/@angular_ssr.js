5f1b4c0d797a9b91eff0471b4ffba0c50f5ff5f1
Raúl Itoiz <63600546+Lauder01@users.noreply.github.com>
2025-08-21 14:12:59 +0200

    Actualizadas dependencias

---
+++ FrontendApp/.angular/cache/20.2.0/FrontendApp/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-BYHBZ3OT.js";
+import "./chunk-Q7XC2MPH.js";
+import "./chunk-3BVMHICG.js";
+import "./chunk-AUJ3JRON.js";
+import "./chunk-R3PYIEAM.js";
+import "./chunk-Y2I5HR7B.js";
+import "./chunk-OIXPQ4QS.js";
+import "./chunk-O5J3CNTX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
