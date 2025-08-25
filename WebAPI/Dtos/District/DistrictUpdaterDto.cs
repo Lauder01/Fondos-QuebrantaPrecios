5161472dab5745163bcfd41b5ed4397ec94c8c05
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-25 14:12:38 +0200

    Cambiado tipo de `Id` a `string` con validación
    Se ha modificado la propiedad `Id` en `DistrictUpdaterDto` para que sea de tipo `string` en lugar de `Guid`. Se ha añadido un atributo de validación `[StringLength]` que asegura que el `Id` tenga exactamente 36 caracteres, junto con un mensaje de error personalizado.

--- WebAPI/Dtos/District/DistrictUpdaterDto.cs
+++ WebAPI/Dtos/District/DistrictUpdaterDto.cs
@@ -6,6 +6,7 @@ namespace WebAPI.Dtos.District
     public class DistrictUpdaterDto : DistrictBaseDto
     {
         [Required(ErrorMessage = "ERR008: El campo Id es obligatorio")]
-        public Guid Id { get; set; }
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR010: El Id debe tener exactamente 36 caracteres")]
+        public string Id { get; set; }
     }
 }
