b2fe9d334ef7fddcc7b340369251b1b0a4d48b68
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-25 13:54:42 +0200

    Actualiza validaciones de FloorId y BuildingId
    Se eliminan las validaciones de rango para las propiedades
    `FloorId` y `BuildingId` en `ApartmentBaseDto`. Ahora,
    ambas propiedades requieren exactamente 36 caracteres
    en lugar de estar restringidas a un rango de 0 a 36.

--- WebAPI/Dtos/Apartment/ApartmentBaseDto.cs
+++ WebAPI/Dtos/Apartment/ApartmentBaseDto.cs
@@ -12,10 +12,10 @@ namespace WebAPI.Dtos.Apartment
         [StringLength(24, MinimumLength = 1, ErrorMessage = "ERR004: La puerta debe tener entre 1 y 24 caracteres")]
         public string Door { get; set; } = string.Empty;
         [Required(ErrorMessage = "ERR005: El campo FloorId es obligatorio")]
-        [Range(0,36, ErrorMessage = "ERR006: El FloorId debe estar entre 0 y 36")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR010: El FloorId debe tener exactamente 36 caracteres")]
         public string FloorId { get; set; } = string.Empty;
         [Required(ErrorMessage = "ERR007: El campo BuildingId es obligatorio")]
-        [Range(0,36, ErrorMessage = "ERR008: El BuildingId debe estar entre 0 y 36")]
+       [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR010: El BuildingId debe tener exactamente 36 caracteres")]
         public string BuildingId { get; set; } = string.Empty;
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
