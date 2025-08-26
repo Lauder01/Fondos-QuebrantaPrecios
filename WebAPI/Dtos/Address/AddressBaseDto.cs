2394bd55dc7a5bec73bfa1d9526e11cd47fd0f73
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-26 11:03:04 +0200

    Actualiza propiedades de dirección a requeridas
    Se han modificado varias propiedades en `AddressBaseDto` para que sean obligatorias utilizando la palabra clave `required`.
    Las propiedades `BuildingId`, `ZipcodeId`, `ConstructedAddress`, `IsApartment`, `Country` y `City` ahora son requeridas.
    La propiedad `ApartmentId` se inicializa a una cadena vacía, pero sigue siendo opcional.
    Además, se han actualizado los mensajes de error para reflejar estos cambios en la validación.

--- WebAPI/Dtos/Address/AddressBaseDto.cs
+++ WebAPI/Dtos/Address/AddressBaseDto.cs
@@ -7,29 +7,30 @@ namespace WebAPI.Dtos.Address
     {
         [Required(ErrorMessage = "ERR001: El campo BuildingId es obligatorio")]
         [Range(0, 36, ErrorMessage = "ERR002: El BuildingId debe estar entre 0 y 36")]
-        public string BuildingId { get; set; } = string.Empty;
+        public required string BuildingId { get; set; }
 
         [Range(0, 36, ErrorMessage = "ERR003: El ApartmentId debe estar entre 0 y 36")]
-        public string? ApartmentId { get; set; }
+        public string? ApartmentId { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "ERR004: El campo ZipcodeId es obligatorio")]
         [StringLength(50, MinimumLength = 1, ErrorMessage = "ERR005: El ZipcodeId debe tener entre 1 y 50 caracteres")]
-        public string ZipcodeId { get; set; } = string.Empty;
+        public required string ZipcodeId { get; set; }
 
         [Required(ErrorMessage = "ERROR006: El campo ZipcodeId es obligatorio")]
         [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR007: La dirección construida debe tener entre 1 y 255 caracteres")]
-        public string ConstructedAddress { get; set; } = string.Empty;
+        public required string ConstructedAddress { get; set; }
 
-        [Range(0, 36, ErrorMessage = "ERR008: El IsApartment debe estar entre 0 y 36")]
-        public bool? IsApartment { get; set; }
+        [Required(ErrorMessage = "ERROR008: El campo IsApartment es obligatorio")]
+        [Range(0, 36, ErrorMessage = "ERR009: El IsApartment debe estar entre 0 y 36")]
+        public required bool IsApartment { get; set; }
 
-        [Required(ErrorMessage = "ERROR09: El campo country es obligatorio")]
-        [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR010: El country debe tener entre 1 y 255 caracteres")]
-        public string Country { get; set; } = string.Empty;
+        [Required(ErrorMessage = "ERROR010: El campo country es obligatorio")]
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR011: El country debe tener entre 1 y 255 caracteres")]
+        public required string Country { get; set; } 
 
-        [Required(ErrorMessage = "ERROR11: El campo state es obligatorio")]
-        [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR012: El state debe tener entre 1 y 255 caracteres")]
-        public string City { get; set; } = string.Empty;
+        [Required(ErrorMessage = "ERROR12: El campo state es obligatorio")]
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR013: El state debe tener entre 1 y 255 caracteres")]
+        public required string City { get; set; } 
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
