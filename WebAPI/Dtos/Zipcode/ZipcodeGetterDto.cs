dfe3b15b40e3f6c8febedd2e7282cb3e7c242618
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:52:02 +0200

    Actualiza mensajes de error en validaciones de DTOs
    Se han modificado los mensajes de error para los campos `Code` e `Id` en las clases `ZipcodeBaseDto`, `ZipcodeGetterDto` y `ZipcodeUpdaterDto`. Ahora utilizan "Código" e "Identificador" para mejorar la claridad y la localización en español.

--- WebAPI/Dtos/Zipcode/ZipcodeGetterDto.cs
+++ WebAPI/Dtos/Zipcode/ZipcodeGetterDto.cs
@@ -5,8 +5,8 @@ namespace WebAPI.Dtos.Zipcode
 {
     public class ZipcodeGetterDto : ZipcodeBaseDto
     {
-        [Required(ErrorMessage = "ERR003: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR004: El campo Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR003: El campo Identificador es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR004: El campo Identificador debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
--- WebAPI/Dtos/Zipcode/ZipcodeUpdaterDto.cs
+++ WebAPI/Dtos/Zipcode/ZipcodeUpdaterDto.cs
@@ -5,8 +5,8 @@ namespace WebAPI.Dtos.Zipcode
 {
     public class ZipcodeUpdaterDto : ZipcodeBaseDto
     {
-        [Required(ErrorMessage = "ERR005: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR006: El campo Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR005: El campo Identificador es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR006: El campo Identificador debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
