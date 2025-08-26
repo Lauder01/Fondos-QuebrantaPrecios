7835fcf73180f845fe54262b031dc951398c76fe
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:21:14 +0200

    Actualiza mensajes de error y nombres de propiedades
    Se han traducido los mensajes de error de las clases
    `StreetBaseDto`, `StreetGetterDto` y `StreetUpdaterDto`
    al inglés y se han hecho más claros. Las propiedades
    "Nombre" y "Código" en `StreetBaseDto` han sido renombradas
    a "Name" y "Code". También se han actualizado los mensajes
    de error para la propiedad `Id` en las clases
    `StreetGetterDto` y `StreetUpdaterDto`.

--- WebAPI/Dtos/Street/StreetBaseDto.cs
+++ WebAPI/Dtos/Street/StreetBaseDto.cs
@@ -5,12 +5,12 @@ namespace WebAPI.Dtos.Street
 {
     public class StreetBaseDto : IValidatableObject
     {
-        [Required(ErrorMessage = "ERR001: El campo Nombre es obligatorio")]
-        [Range(0,255, ErrorMessage = "ERR002: El nombre debe estar entre 0 y 255")]
+        [Required(ErrorMessage = "ERR001: El campo Name es obligatorio")]
+        [Range(0,255, ErrorMessage = "ERR002: El campo Name debe estar entre 0 y 255")]
         public required string Name { get; set; }
 
-        [Required(ErrorMessage = "ERR003: El campo Código es obligatorio")]
-        [Range(0,24, ErrorMessage = "ERR004: El código debe estar entre 0 y 24")]
+        [Required(ErrorMessage = "ERR003: El campo Code es obligatorio")]
+        [Range(0,24, ErrorMessage = "ERR004: El campo Code debe estar entre 0 y 24")]
         public required string Code { get; set; }
 
         [Required(ErrorMessage = "ERR005: El campo StreetType es obligatorio")]
--- WebAPI/Dtos/Street/StreetGetterDto.cs
+++ WebAPI/Dtos/Street/StreetGetterDto.cs
@@ -5,7 +5,7 @@ namespace WebAPI.Dtos.Street
     public class StreetGetterDto : StreetBaseDto
     {
         [Required(ErrorMessage = "ERR006: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR007: El Id debe tener exactamente 36 caracteres")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR007: El campo Id debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
