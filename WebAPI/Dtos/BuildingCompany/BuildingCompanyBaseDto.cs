6d51707ac39fe3dbec23fe3cca4415b910b1af0f
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:33:12 +0200

    Actualiza mensajes de error y nombres de campos
    Se han traducido los mensajes de error al español y se han modificado los nombres de los campos en las clases `BuildingCompanyBaseDto`, `BuildingCompanyGetterDto` y `BuildingCompanyUpdaterDto` para ser más descriptivos. Los campos "Name", "Cif" e "Id" ahora son "nombre", "CIF" e "identificador", respectivamente. Las validaciones de longitud y requerimiento se han mantenido.

--- WebAPI/Dtos/BuildingCompany/BuildingCompanyBaseDto.cs
+++ WebAPI/Dtos/BuildingCompany/BuildingCompanyBaseDto.cs
@@ -5,16 +5,16 @@ namespace WebAPI.Dtos.BuildingCompany
 {
     public class BuildingCompanyBaseDto : IValidatableObject
     {
-        [Required(ErrorMessage = "ERR001: El campo Name es obligatorio")]
-        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El campo Name debe tener entre 2 y 255 caracteres")]
+        [Required(ErrorMessage = "ERR001: El campo nombre es obligatorio")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El campo nombre debe tener entre 2 y 255 caracteres")]
         public required string Name { get; set; }
 
-        [Required(ErrorMessage = "ERR003: El campo Cif es obligatorio")]
-        [StringLength(9, MinimumLength = 9, ErrorMessage = "ERR004: El campo Cif debe tener 9 caracteres")]
+        [Required(ErrorMessage = "ERR003: El campo CIF es obligatorio")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "ERR004: El campo CIF debe tener 9 caracteres")]
         public required string Cif { get; set; } 
 
-        [StringLength(1024, ErrorMessage = "ERR005: El campo Website no puede superar los 1024 caracteres")]
-        [Url(ErrorMessage = "ERR006: El campo Website debe ser una URL válida")]
+        [StringLength(1024, ErrorMessage = "ERR005: El campo sitio web no puede superar los 1024 caracteres")]
+        [Url(ErrorMessage = "ERR006: El campo sitio web debe ser una URL válida")]
         public string? Website { get; set; }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
