fdd718ffa7d81129199bf632ef7048da026f3e4f
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:31:15 +0200

    Actualiza mensajes de error en validaciones DTOs
    Se han traducido y mejorado los mensajes de error en las validaciones de los campos de las clases `ApartmentBaseDto`, `ApartmentGetterDto` y `ApartmentUpdaterDto`. Se han realizado cambios en términos como "Code" a "código", "Door" a "puerta" y "FloorId" a "identificador de piso", así como "Id" a "identificador". Esto mejora la claridad y consistencia del lenguaje utilizado.

--- WebAPI/Dtos/Apartment/ApartmentGetterDto.cs
+++ WebAPI/Dtos/Apartment/ApartmentGetterDto.cs
@@ -5,8 +5,8 @@ namespace WebAPI.Dtos.Apartment
 {
     public class ApartmentGetterDto : ApartmentBaseDto
     {
-        [Required(ErrorMessage = "ERR007: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR008: El campo Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR007: El campo identificador es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR008: El campo identificador debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
