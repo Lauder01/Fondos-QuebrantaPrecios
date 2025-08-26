4ae2b537db7188a4582f1f96a13e27d722ea0b0e
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 12:31:42 +0200

    Subir cambios "Stagged" de FrontendApp, y cambios para actualizar los campos 'required' en "PurchaseBaseDto.cs",  "PurchaseGetterDto.cs" y "PurchaseUpdaterDto.cs".

--- WebAPI/Dtos/Purchase/PurchaseUpdaterDto.cs
+++ WebAPI/Dtos/Purchase/PurchaseUpdaterDto.cs
@@ -4,8 +4,8 @@ namespace WebAPI.Dtos.Purchase
 {
     public class PurchaseUpdaterDto : PurchaseBaseDto
     {
-        [Required(ErrorMessage = "ERR007: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR010: El Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR011: El campo Id es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR012: El Id debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
