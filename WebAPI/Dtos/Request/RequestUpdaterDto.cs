5a880dc132c4efd3e0b38b635ca110e4242a7252
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 12:20:49 +0200

    Mejora de validación y limpieza de código en DTOs
    Se han realizado cambios significativos en el código, incluyendo la adición de nuevas funcionalidades y la eliminación de código obsoleto, lo que mejora la eficiencia y la mantenibilidad del proyecto.
    
    Se han actualizado las clases `RequestBaseDto`, `RequestGetterDto` y `RequestUpdaterDto` para mejorar la validación de los campos. Se han añadido mensajes de error personalizados para las anotaciones de validación, proporcionando mejor retroalimentación al usuario. Además, se ha cambiado la propiedad `BuildingId`, `StatusId`, `Price`, `MaintenancePrice` e `Id` para que sean obligatorias utilizando la palabra clave `required`.

--- WebAPI/Dtos/Request/RequestUpdaterDto.cs
+++ WebAPI/Dtos/Request/RequestUpdaterDto.cs
@@ -4,8 +4,8 @@ namespace WebAPI.Dtos.Request
 {
     public class RequestUpdaterDto : RequestBaseDto
     {
-        [Required]
-        [Range(0,36)]
-        public string Id { get; set; } = string.Empty;
+        [Required(ErrorMessage = "ERR009: El campo Id es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR010: El Id debe tener exactamente 36 caracteres")]
+        public required string Id { get; set; } = string.Empty;
     }
 }
