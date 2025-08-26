f3df496061e168d1d449e52e5389019c35eb1285
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 12:46:59 +0200

    Actualiza DTOs para usar propiedades requeridas
    Se ha modificado `StreetBaseDto` para utilizar la sintaxis de propiedades requeridas en C# 11. Las propiedades `Name`, `Code` y `StreetType` ahora son requeridas y se han actualizado los mensajes de error.
    
    En `StreetGetterDto.cs` y `StreetUpdaterDto.cs`, la propiedad `Id` se ha hecho requerida y se ha añadido una validación de longitud exacta de 36 caracteres, junto con mensajes de error más descriptivos.

--- WebAPI/Dtos/Street/StreetBaseDto.cs
+++ WebAPI/Dtos/Street/StreetBaseDto.cs
@@ -7,12 +7,14 @@ namespace WebAPI.Dtos.Street
     {
         [Required(ErrorMessage = "ERR001: El campo Nombre es obligatorio")]
         [Range(0,255, ErrorMessage = "ERR002: El nombre debe estar entre 0 y 255")]
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
+
         [Required(ErrorMessage = "ERR003: El campo Código es obligatorio")]
         [Range(0,24, ErrorMessage = "ERR004: El código debe estar entre 0 y 24")]
-        public string Code { get; set; } = string.Empty;
+        public required string Code { get; set; }
+
         [Required(ErrorMessage = "ERR005: El campo StreetType es obligatorio")]
-        public int StreetType { get; set; }
+        public required int StreetType { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
