ded63fb6f424a8b42e08be8957f4368ce4634cbf
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:35:47 +0200

    Actualiza nombres de campos y mensajes de error
    Se han traducido los nombres de los campos en las clases `DistrictBaseDto`, `DistrictGetterDto` y `DistrictUpdaterDto` al español. Los campos "Name", "Code", "Country", "City" e "Id" ahora se llaman "Nombre", "Código", "País", "Ciudad" e "Identificador", respectivamente. También se han ajustado los mensajes de error para reflejar estos cambios.

--- WebAPI/Dtos/District/DistrictBaseDto.cs
+++ WebAPI/Dtos/District/DistrictBaseDto.cs
@@ -5,18 +5,18 @@ namespace WebAPI.Dtos.District
 {
     public class DistrictBaseDto : IValidatableObject
     {
-        [Required(ErrorMessage = "ERR001: El campo Name es obligatorio")]
-        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El campo Name debe tener entre 2 y 255 caracteres")]
+        [Required(ErrorMessage = "ERR001: El campo Nombre es obligatorio")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El campo Nombre debe tener entre 2 y 255 caracteres")]
         public required string Name { get; set; } 
 
-        [Required(ErrorMessage = "ERR003: El campo Code es obligatorio")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "ERR004: El campo Code debe tener entre 2 y 50 caracteres")]
+        [Required(ErrorMessage = "ERR003: El campo Código es obligatorio")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "ERR004: El campo Código debe tener entre 2 y 50 caracteres")]
         public required string Code { get; set; } 
 
-        [StringLength(255, ErrorMessage = "ERR005: El campo Country no puede superar los 255 caracteres")]
+        [StringLength(255, ErrorMessage = "ERR005: El campo País no puede superar los 255 caracteres")]
         public string? Country { get; set; }
 
-        [StringLength(255, ErrorMessage = "ERR006: El campo City no puede superar los 255 caracteres")]
+        [StringLength(255, ErrorMessage = "ERR006: El campo Ciudad no puede superar los 255 caracteres")]
         public string? City { get; set; }
 
         public List<string> Zipcodes { get; set; } = new List<string>();
--- WebAPI/Dtos/District/DistrictGetterDto.cs
+++ WebAPI/Dtos/District/DistrictGetterDto.cs
@@ -6,8 +6,8 @@ namespace WebAPI.Dtos.District
 {
     public class DistrictGetterDto : DistrictBaseDto
     {
-        [Required(ErrorMessage = "ERR007: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR008: El campo Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR007: El campo Identificador es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR008: El campo Identificador debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
         // Hereda Zipcodes del DistrictBaseDto
     }
