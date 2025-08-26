be77347384153f635fc9600453155142d08fdb86
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:50:51 +0200

    Actualiza mensajes de error y traducciones en DTOs
    Se han modificado las clases `StreetBaseDto`, `StreetGetterDto` y `StreetUpdaterDto` para traducir los nombres de los campos al español y actualizar los mensajes de error de las validaciones. Los campos "Name", "Code", "StreetType" e "Id" ahora se llaman "Nombre", "Código", "Tipo de Calle" e "Identificador", respectivamente.

--- WebAPI/Dtos/Street/StreetBaseDto.cs
+++ WebAPI/Dtos/Street/StreetBaseDto.cs
@@ -5,15 +5,15 @@ namespace WebAPI.Dtos.Street
 {
     public class StreetBaseDto : IValidatableObject
     {
-        [Required(ErrorMessage = "ERR001: El campo Name es obligatorio")]
-        [Range(0,255, ErrorMessage = "ERR002: El campo Name debe estar entre 0 y 255")]
+        [Required(ErrorMessage = "ERR001: El campo Nombre es obligatorio")]
+        [Range(0,255, ErrorMessage = "ERR002: El campo Nombre debe estar entre 0 y 255")]
         public required string Name { get; set; }
 
-        [Required(ErrorMessage = "ERR003: El campo Code es obligatorio")]
-        [Range(0,24, ErrorMessage = "ERR004: El campo Code debe estar entre 0 y 24")]
+        [Required(ErrorMessage = "ERR003: El campo Código es obligatorio")]
+        [Range(0,24, ErrorMessage = "ERR004: El campo Código debe estar entre 0 y 24")]
         public required string Code { get; set; }
 
-        [Required(ErrorMessage = "ERR005: El campo StreetType es obligatorio")]
+        [Required(ErrorMessage = "ERR005: El campo Tipo de Calle es obligatorio")]
         public required int StreetType { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
--- WebAPI/Dtos/Street/StreetGetterDto.cs
+++ WebAPI/Dtos/Street/StreetGetterDto.cs
@@ -4,8 +4,8 @@ namespace WebAPI.Dtos.Street
 {
     public class StreetGetterDto : StreetBaseDto
     {
-        [Required(ErrorMessage = "ERR006: El campo Id es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR007: El campo Id debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR006: El campo Identificador es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR007: El campo Identificador debe tener exactamente 36 caracteres")]
         public required string Id { get; set; }
     }
 }
