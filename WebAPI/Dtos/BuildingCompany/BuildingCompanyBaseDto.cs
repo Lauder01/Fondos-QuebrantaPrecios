7cac55914c17621b6df40a4ec346b257112cc011
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:04:13 +0200

    Actualiza validaciones en DTOs de empresa constructora
    Se han modificado las clases `BuildingCompanyBaseDto`, `BuildingCompanyGetterDto` y `BuildingCompanyUpdaterDto` para incluir el modificador `required` en las propiedades `Name`, `Cif`, `Website` e `Id`. Se han ajustado los mensajes de error para que reflejen correctamente los nombres de los campos. Además, el campo `Website` ahora permite valores nulos al ser de tipo `string?`.

--- WebAPI/Dtos/BuildingCompany/BuildingCompanyBaseDto.cs
+++ WebAPI/Dtos/BuildingCompany/BuildingCompanyBaseDto.cs
@@ -5,14 +5,16 @@ namespace WebAPI.Dtos.BuildingCompany
 {
     public class BuildingCompanyBaseDto : IValidatableObject
     {
-        [Required(ErrorMessage = "ERR001: El campo Nombre es obligatorio")]
-        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El nombre debe tener entre 2 y 255 caracteres")]
-        public string Name { get; set; } = string.Empty;
-        [Required(ErrorMessage = "ERR003: El campo CIF es obligatorio")]
-        [StringLength(9, MinimumLength = 9, ErrorMessage = "ERR004: El CIF debe tener 9 caracteres")]
-        public string Cif { get; set; } = string.Empty;
-        [StringLength(1024, ErrorMessage = "ERR005: La web no puede superar los 1024 caracteres")]
-        [Url(ErrorMessage = "ERR006: La web debe ser una URL válida")]
+        [Required(ErrorMessage = "ERR001: El campo Name es obligatorio")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "ERR002: El campo Name debe tener entre 2 y 255 caracteres")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "ERR003: El campo Cif es obligatorio")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "ERR004: El campo Cif debe tener 9 caracteres")]
+        public required string Cif { get; set; } 
+
+        [StringLength(1024, ErrorMessage = "ERR005: El campo Website no puede superar los 1024 caracteres")]
+        [Url(ErrorMessage = "ERR006: El campo Website debe ser una URL válida")]
         public string? Website { get; set; }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
