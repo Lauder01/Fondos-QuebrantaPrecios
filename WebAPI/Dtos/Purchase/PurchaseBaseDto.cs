214f3490588aacb0d2748889a175b02e8e1c525f
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-26 13:46:31 +0200

    Actualiza mensajes de error y traducciones en DTOs
    Se han traducido los nombres de los campos y mensajes de error en las clases `PurchaseBaseDto`, `PurchaseGetterDto` y `PurchaseUpdaterDto` al español. Las validaciones de longitud y requerimiento se han mantenido. Este cambio mejora la localización del código.

--- WebAPI/Dtos/Purchase/PurchaseBaseDto.cs
+++ WebAPI/Dtos/Purchase/PurchaseBaseDto.cs
@@ -5,22 +5,22 @@ namespace WebAPI.Dtos.Purchase
 {
     public class PurchaseBaseDto
     {
-        [Required(ErrorMessage = "ERR001: El campo BuildingId es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR002: El campo BuildingId debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR001: El campo Identificador de Edificio es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR002: El campo Identificador de Edificio debe tener exactamente 36 caracteres")]
         public required string BuildingId { get; set; }
 
-        [Required(ErrorMessage = "ERR003: El campo BuildingCompanyId es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR004: El campo BuildingCompanyId debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR003: El campo Identificador de Empresa Constructora es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR004: El campo Identificador de Empresa Constructora debe tener exactamente 36 caracteres")]
         public required string BuildingCompanyId { get; set; }
 
-        [Required(ErrorMessage = "ERR005: El campo RequestId es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR006: El campo RequestId debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR005: El campo Identificador de Pedido es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR006: El campo Identificador de Pedido debe tener exactamente 36 caracteres")]
         public required string RequestId { get; set; } 
 
-        [Required(ErrorMessage = "ERR007: El campo Date es obligatorio")]
+        [Required(ErrorMessage = "ERR007: El campo Fecha es obligatorio")]
         public required DateTime Date { get; set; }
 
-        [Required(ErrorMessage = "ERR008: El campo Amount es obligatorio")]
+        [Required(ErrorMessage = "ERR008: El campo Cantidad es obligatorio")]
         [Range(0.0, 9999999999.99)]
         public required decimal Amount { get; set; }
     }
