e034541ca5cdc96645a7028285988839680dcb2a
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-27 10:21:49 +0200

    Mejora de validación en DTOs de imágenes de edificios
    Se han realizado cambios significativos en el código, incluyendo la adición de nuevas funcionalidades y la eliminación de código obsoleto, lo que mejora la eficiencia y la mantenibilidad del proyecto.
    
    Se han actualizado las clases `BuildingImageGetterDto` y `BuildingImageUpdaterDto` para mejorar la validación del campo `BuildingImageId`. Se han añadido atributos de validación con mensajes de error personalizados para asegurar que el campo sea obligatorio y que su longitud sea exactamente de 36 caracteres. Además, se ha cambiado el tipo de propiedad a `required` para indicar que este campo es obligatorio en ambas clases.

--- WebAPI/Dtos/BuildingImage/BuildingImageUpdaterDto.cs
+++ WebAPI/Dtos/BuildingImage/BuildingImageUpdaterDto.cs
@@ -4,8 +4,8 @@ namespace WebAPI.Dtos.BuildingImage
 {
     public class BuildingImageUpdaterDto : BuildingImageBaseDto
     {
-        [Required]
-        [StringLength(36, MinimumLength = 36)]
-        public string BuildingImageId { get; set; }
+        [Required(ErrorMessage = "ERR010: El campo Identificador de Imagen de Edificio es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR011: El campo Identificador de Imagen de Edificio debe tener exactamente 36 caracteres")]
+        public required string BuildingImageId { get; set; }
     }
 }
