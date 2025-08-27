dc8c401b41333a99834ff533e52cccabea8fb2cd
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-27 09:22:37 +0200

    si

--- FrontendApp/src/app/app.component.ts
+++ FrontendApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   template: `
     <app-menu></app-menu>
     <div class="container mt-4">
