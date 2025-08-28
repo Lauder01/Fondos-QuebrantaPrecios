b971131e7a394a9bfb5f9d20163920304bf95b03
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-28 12:25:05 +0200

    HasLift eliminado de la entidad Floor

--- RepositoryLibraryProject/Data/AppDbContext.cs
+++ RepositoryLibraryProject/Data/AppDbContext.cs
@@ -388,9 +388,6 @@ public partial class AppDbContext : DbContext
                 .IsRequired()
                 .HasMaxLength(36)
                 .IsUnicode(false);
-            entity.Property(e => e.HasLift)
-                .IsRequired()
-                .HasDefaultValueSql("((1))");
             entity.Property(e => e.Code)
                 .IsRequired()
                 .HasMaxLength(30)
--- WebAPI/Dtos/Floor/FloorBaseDto.cs
+++ WebAPI/Dtos/Floor/FloorBaseDto.cs
@@ -9,9 +9,6 @@ namespace WebAPI.Dtos.Floor
         [Range(-12, 100, ErrorMessage = "ERR002: El campo Número de Piso debe estar entre -12 y 100")]
         public required int FloorNumber { get; set; }
         
-        [Required(ErrorMessage = "ERR003: El campo Tiene Ascensor es obligatorio")]
-        public required bool HasLift { get; set; }
-        
         [Required(ErrorMessage = "ERR004: El campo Identificador de Edificio es obligatorio")]
         [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR005: El campo Identificador de Edificio debe tener exactamente 36 caracteres")]
         public required string BuildingId { get; set; }
