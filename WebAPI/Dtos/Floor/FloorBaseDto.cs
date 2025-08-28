49c7c64d81091c8dd9d3cbd277fb4b60044b40b4
diegoiriarte138 <diegoiriarte138@gmail.com>
2025-08-28 12:32:32 +0200

    .

--- WebAPI/Dtos/Floor/FloorBaseDto.cs
+++ WebAPI/Dtos/Floor/FloorBaseDto.cs
@@ -9,8 +9,8 @@ namespace WebAPI.Dtos.Floor
         [Range(-12, 100, ErrorMessage = "ERR002: El campo Número de Piso debe estar entre -12 y 100")]
         public required int FloorNumber { get; set; }
         
-        [Required(ErrorMessage = "ERR004: El campo Identificador de Edificio es obligatorio")]
-        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR005: El campo Identificador de Edificio debe tener exactamente 36 caracteres")]
+        [Required(ErrorMessage = "ERR003: El campo Identificador de Edificio es obligatorio")]
+        [StringLength(36, MinimumLength = 36, ErrorMessage = "ERR004: El campo Identificador de Edificio debe tener exactamente 36 caracteres")]
         public required string BuildingId { get; set; }
         
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
