540aabe0c53608e1ea5fc9d4f589a5170f63e8b4
Javier Zaratiegui <javizaraurda@outlook.com>
2025-08-28 10:49:44 +0200

    Añadir perfil de AutoMapper y DTO para edificios
    Se ha creado un nuevo perfil de AutoMapper `SpecuLabProfile` que mapea la entidad `Building` a `SpecuLabCreatorDto`, incluyendo propiedades como `BuildingCode`, `BuildingName`, `Address`, `District`, `FloorCount` y `YearBuilt`. Además, se ha implementado el DTO `SpecuLabCreatorDto` con propiedades requeridas para representar la información de un edificio, manteniendo la opción de incluir `ApartmentNumber` en el futuro.

---
+++ WebAPI/AutoMapperProfiles/SpecuLabProfile.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using ClassLibraryProject.Entities;
+using WebAPI.Dtos.SpecuLab;
+using System.Linq;
+
+namespace WebAPI.AutoMapperProfiles
+{
+    public class SpecuLabProfile : Profile
+    {
+        public SpecuLabProfile()
+        {
+            CreateMap<Building, SpecuLabCreatorDto>()
+                .ForMember(dest => dest.BuildingCode, opt => opt.MapFrom(src => src.Code))
+                .ForMember(dest => dest.BuildingName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address.FirstOrDefault() != null ? src.Address.FirstOrDefault().ConstructedAddress : string.Empty))
+                .ForMember(dest => dest.District, opt => opt.MapFrom(src => src.District != null ? src.District.Name : string.Empty))
+                .ForMember(dest => dest.FloorCount, opt => opt.MapFrom(src => src.FloorCount))
+                .ForMember(dest => dest.YearBuilt, opt => opt.MapFrom(src => src.YearBuilt));
+                // .ForMember(dest => dest.ApartmentNumber, opt => opt.Ignore()); // Para uso futuro
+        }
+    }
+}
