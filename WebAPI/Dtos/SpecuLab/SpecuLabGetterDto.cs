6b63e2241f44c96b5a9a3bfa004946014fb6ca6b
javiuz <javizaraurda@outlook.com>
2025-09-03 12:41:49 +0200

    Actualiza validaciones en SpecuLabCreatorDto y SpecuLabGetterDto
    Se han modificado los mensajes y códigos de error para los campos `BuildingCode`, `BuildingName`, `ConstructedAddress`, `DistrictName`, `FloorCount`, `YearBuilt` y `ApartmentCount` en `SpecuLabCreatorDto`. En `SpecuLabGetterDto`, se han añadido atributos de validación a los mismos campos. También se han corregido caracteres especiales en los mensajes de error.

--- WebAPI/Dtos/SpecuLab/SpecuLabGetterDto.cs
+++ WebAPI/Dtos/SpecuLab/SpecuLabGetterDto.cs
@@ -7,11 +7,28 @@ namespace WebAPI.Dtos.SpecuLab
 {
     public class SpecuLabGetterDto
     {
-        public string BuildingName { get; set; }
-        public string ConstructedAddress { get; set; }
-        public string DistrictName { get; set; }
-        public int FloorCount { get; set; }
-        public int YearBuilt { get; set; }
-        public int ApartmentCount { get; set; }
+        [Required(ErrorMessage = "ERR001: El campo Nombre de Edificio es obligatorio")]
+        [StringLength(255, MinimumLength = 0, ErrorMessage = "ERR002: El campo Nombre de Edificio debe tener entre 0 y 255 caracteres")]
+        public required string BuildingName { get; set; }
+
+        [Required(ErrorMessage = "ERR003: El campo Dirección de Construcción es obligatorio")]
+        [StringLength(255, MinimumLength = 0, ErrorMessage = "ERR004: El campo Dirección de Construcción debe tener entre 0 y 255 caracteres")]
+        public required string ConstructedAddress { get; set; }
+
+        [Required(ErrorMessage = "ERR005: El campo Nombre de Distrito es obligatorio")]
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "ERR006: El campo Nombre de Distrito debe tener entre 1 y 255 caracteres")]
+        public required string DistrictName { get; set; }
+
+        [Required(ErrorMessage = "ERR007: El campo Número de Plantas es obligatorio")]
+        [Range(0, 1000, ErrorMessage = "ERR008: El campo Número de Plantas debe estar entre 0 y 1000")]
+        public required int FloorCount { get; set; }
+
+        [Required(ErrorMessage = "ERR009: El campo Año de Construcción es obligatorio")]
+        [Range(1800, 2100, ErrorMessage = "ERR010: El campo Año de Construcción debe estar entre 1800 y 2100")]
+        public required int YearBuilt { get; set; }
+
+        [Required(ErrorMessage = "ERR011: El campo Número de Apartamentos es obligatorio")]
+        [Range(0, 1000, ErrorMessage = "ERR012: El campo Número de Apartamentos debe estar entre 0 y 1000")]
+        public required int ApartmentCount { get; set; }
     }
 }
